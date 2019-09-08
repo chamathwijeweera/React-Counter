d8bbceef4664d27ac81d196b05df9c386e626975
chamathwijeweera <chamathre@gmail.com>
2019-09-08 19:52:12 +0530

    counters componenet added

---
+++ src/components/counter.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: []
+    //imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
+
+  render() {
+    console.log("props", this.props);
+
+    return (
+      <div>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>{this.renderTags()}</ul>
+      </div>
+    );
+  }
+
+  handleIncrement = () => {
+    //console.log("Increment clicked.");
+
+    this.setState({ count: this.state.count + 1 });
+  };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    else return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
